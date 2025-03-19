934b426c60424fb34a0ac8630d8cc4db32611585
Aaditree Jaisswal <adi18rox@gmail.com>
2025-03-18 23:04:47 -0400

    created popup for contact info

--- script.js
+++ script.js
@@ -27,4 +27,15 @@ icon.classList.toggle("open");
       });
   });
   
+const openBtn = document.getElementById ("openModal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById ("modal" );
+
+openBtn.addEventListener("click", () => {
+modal.classList.add ("open");
+});
+
+closeBtn.addEventListener("click", () => {
+modal.classList.remove("open");
+});
   
